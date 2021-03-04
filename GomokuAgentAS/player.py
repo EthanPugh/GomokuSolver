6aff4c4d30313e0dfa6acda2c6e9318506f4e169
mitch <mjames962@hotmail.com>
2021-03-04 11:55:54 +0000

    Code cleanup and comments

--- GomokuAgentAS/player.py
+++ GomokuAgentAS/player.py
@@ -1,13 +1,17 @@
-import numpy as np
+# Minimax Gomoku Agent
+#
+# Mitchell James 981988
+# Ethan Pugh 985094
+# Swansea University
 
-from misc import legalMove
+import numpy as np
 from gomokuAgent import GomokuAgent
+from misc import legalMove
 
 
 class Player(GomokuAgent):
 
     def move(self, board):
-        # moveLoc = tuple(np.random.randint(self.BOARD_SIZE, size=2)) random move
         return self.minimax_decision(board)
 
     def minimax_decision(self, board):
@@ -20,9 +24,6 @@ class Player(GomokuAgent):
     def max_value(self, board):
         # initialize v = -∞
         v = -np.inf
-        moveLoc = (0, 0)
-        ##while not legalMove(board, moveLoc):
-        ##  moveLoc = tuple(np.random.randint(self.BOARD_SIZE, size=2))
 
         coords = self.get_successors(board)
 
@@ -36,7 +37,7 @@ class Player(GomokuAgent):
 
             state_val = self.value(temp)
 
-            if state_val > v:
+            if state_val > v and legalMove(board, (r, c)):
                 v = state_val
                 moveLoc = (r, c)
 
@@ -45,9 +46,6 @@ class Player(GomokuAgent):
     def min_value(self, board):
         # initialize v = +∞
         v = np.inf
-        moveLoc = (0, 0)
-        ##while not legalMove(board, moveLoc):
-        ##  moveLoc = tuple(np.random.randint(self.BOARD_SIZE, size=2))
 
         coords = self.get_successors(board)
 
@@ -62,7 +60,7 @@ class Player(GomokuAgent):
 
             state_val = self.value(temp)
 
-            if state_val < v:
+            if state_val < v and legalMove(board, (r, c)):
                 v = state_val
                 moveLoc = (r, c)
 
@@ -71,7 +69,6 @@ class Player(GomokuAgent):
     def value(self, board):
         # return value of position
         # Terminal positions are filled boards
-
         return self.row_weight(board) + self.diag_weight(board)
 
     def row_weight(self, board):
@@ -81,15 +78,14 @@ class Player(GomokuAgent):
 
         for r in range(BOARD_SIZE):
             for c in range(BOARD_SIZE - X_IN_A_LINE + 1):
-                opponent_combo = 0
 
                 for i in range(X_IN_A_LINE):
 
                     if board[r, c + i] != 1:  # player 1's vertical chain of length (i + 1) starting at r, c
-                        sum_weight = sum_weight + pow(i + 1, 2)
+                        sum_weight = sum_weight + pow(i, 2)
 
                     if board[r, c + i] != -1:  # player 2's vertical chain of length (i + 1) starting at r, c
-                        sum_weight = sum_weight - pow(i + 1, 2)
+                        sum_weight = sum_weight - pow(i, 2)
 
         return sum_weight
 
@@ -111,19 +107,12 @@ class Player(GomokuAgent):
         return sum_weight
 
     def get_successors(self, board):
-        successors = np.array(board)
         coords = np.array([[0, 0]])
-
         BOARD_SIZE = self.BOARD_SIZE
 
         for r in range(BOARD_SIZE):
             for c in range(BOARD_SIZE):
                 if board[r, c] == 0:
-                    # temp = np.array(board)
-                    # temp = np.copy(board)
-                    # temp[r, c] = self.ID
-                    # successors = np.append(successors, np.array(temp))
                     coords = np.append(coords, np.array([[r, c]]), axis=0)
 
-        # print(board)
         return np.delete(coords, 0, 0)
